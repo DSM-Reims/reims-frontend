615c90078a26bc631c8ba7c580bcb46e6fed5005
iqamac <hyun77320@naver.com>
2022-05-30 15:53:18 +0900

    update

--- src/component/common/Body/index.js
+++ src/component/common/Body/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ThumbnailExplanation from './ThumbnailExplanation';
 import { ReactComponent as LeftArrow } from '../../../assets/chevron-left-solid.svg'
 import { ReactComponent as RightArrow } from '../../../assets/chevron-right-solid.svg'
 
 const Body = () => {
+    const [callback, setCallback] = useState([]);
+
     return (
         <Wrapper>
             <div>
-                <LeftArrow width='50' height='50' />
-                <ThumbnailExplanation />
-                <RightArrow width='50' height='50' />
+                <LeftArrow onClick={() => callback[0]()} id='left-arrow' width='50' height='50' />
+                <ThumbnailExplanation setCallback={setCallback} />
+                <RightArrow onClick={() => callback[1]()} id='right-arrow' width='50' height='50' />
             </div>
         </Wrapper>
     );
@@ -29,5 +31,15 @@ const Wrapper = styled.section`
         display: flex;
         justify-content: space-between;
         align-items: center;
+        #left-arrow{
+            :hover{
+                cursor: pointer;
+            }
+        }
+        #right-arrow{
+            :hover{
+                cursor: pointer;
+            }
+        }
     }
 `;
